902ffbd7838ebeb16d701e23d90e8b7a5b4ca704
Willem Rutten <w.j.rutten@student.tue.nl>
2017-03-16 15:27:44 +0100

    no message

--- test_willem.m
+++ test_willem.m
@@ -1,9 +1,22 @@
 %%willem test
+n=0
 figure;
-rand('twister',1);
-X = randn(10,10);
-imagesc(X);
-colorbar
-colormapeditor
+while n<10
+    rand('twister',1);
+    X = randn(10,10);
+    imagesc(X);
+    colorbar;
+    drawnow;
+    n=n+1
+end
 
-
+% h = animatedline;
+% axis([0 4*pi -1 1])
+% x = linspace(0,4*pi,2000);
+% 
+% for k = 1:length(x)
+%     y = sin(x(k));
+%     addpoints(h,x(k),y);
+%     drawnow
+% end
+% 
